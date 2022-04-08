57ee8b17dc02d4ae88c3b38526e60b3f0cbab9f8
mayuresh-byte <70745530+mayuresh-byte@users.noreply.github.com>
2022-04-08 12:29:10 +0530

    8april

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, )
+    path('', views.index),
+    path('login/', views.login),
+    path('register/', views.register),
 ]
 
